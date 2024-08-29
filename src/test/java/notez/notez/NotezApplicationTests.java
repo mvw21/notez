dfa1be1acff69ffda468edad940684340f4e8904
mvw21 <nexusww21@gmail.com>
2024-08-29 21:33:46 +0300

    initial commit

---
+++ src/test/java/notez/notez/NotezApplicationTests.java
@@ -0,0 +1,13 @@
+package notez.notez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
